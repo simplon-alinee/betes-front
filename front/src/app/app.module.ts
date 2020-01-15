9de0a2066fd94718deb725334c043cbe24a17e06
Aline <a.elwert@hotmail.fr>
2020-01-15 12:03:19 +0100

    Aline -- added router outlet and home component

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { InscriptionComponent } from './components/inscription/inscription.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule} from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    InscriptionComponent
+    InscriptionComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
