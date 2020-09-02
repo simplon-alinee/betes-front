4cdab28b1b2dfd4317337ca2a9174373a399367a
Psyga <florian.ville63@gmail.com>
2020-09-02 12:19:38 +0200

    refonte écran accueil - jeux auto

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {MatchesService} from '../services/matches/matches.service';
+import {Game} from '../models/game.model';
+import {GameService} from '../services/game/game.service';
 
 @Component({
   selector: 'app-products-table',
@@ -7,10 +9,16 @@ import {MatchesService} from '../services/matches/matches.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  gameList: Game[];
 
-  constructor() {
+  constructor(private gameService: GameService) {
   }
 
   ngOnInit() {
+    this.gameService.getGames().subscribe(games => {
+      this.gameList = games.content;
+      console.log(this.gameList);
+    });
   }
+
 }
